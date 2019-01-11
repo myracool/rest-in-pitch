e644642a09196a14abcb2a56a2efbdcd301d1135
Marc Saint-Jean-Clergeau <saintjeanclergeau.marc@gmail.com>
2019-01-11 15:18:29 +0100

    Responses

--- src/m2gl/rip/User.java
+++ src/m2gl/rip/User.java
@@ -5,6 +5,7 @@ import org.bson.Document;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -58,7 +59,26 @@ public class User {
 		return null;
 	}
 	
-	
+	public String login(){
+	    MongoClient mongoClient = new MongoClient();
+		try {
+		    MongoDatabase db = mongoClient.getDatabase("RIP");
+		    MongoCollection<Document> collection = db.getCollection("User");
+
+		    ObjectMapper mapper = new MyObjectMapperProvider().getContext(User.class);
+		    String jsonString = mapper.writeValueAsString(this);
+		    Document doc = Document.parse(jsonString);
+		    FindIterable<Document> res = collection.find(doc);
+		    if (res.iterator().hasNext()) {
+		    	return this.getUsername() + " successfully logged in.";
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
+		} finally {
+		    mongoClient.close();
+		}
+		return null;
+	}
 	
 	
 }
--- src/m2gl/rip/UserResource.java
+++ src/m2gl/rip/UserResource.java
@@ -5,15 +5,35 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("")
 public class UserResource {
 	@POST
-	@Path("/user")
+	@Path("/user/register")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-    public User addUser(User user) {
-		user.saveToDb();
-		return user;
+    public Response addUser(User user) {
+		String res = user.saveToDb();
+		if (res != null) {
+			return Response.ok(res).build();
+		}
+		else {
+			return Response.status(Response.Status.CONFLICT).entity("User already exists").build();
+		}
+    }
+	
+	@POST
+	@Path("/user/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    public Response checkUser(User user) {
+		String res = user.login();
+		if (res != null) {
+			return Response.ok(res).build();
+		}
+		else {
+			return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+		}
     }
 }
